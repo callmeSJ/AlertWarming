ea70bd282fc0ee137a4e70e046be8ac485c8a2d1
apple <sj@sj.local>
2016-05-24 11:21:37 +0800

    sj

--- AlertWarming/ViewController.m
+++ AlertWarming/ViewController.m
@@ -17,8 +17,18 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
 }
-
+-(void)viewDidAppear:(BOOL)animated{
+    UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"SJ" message:@"信息" preferredStyle:UIAlertControllerStyleAlert];
+    
+    UIAlertAction *okAction = [UIAlertAction actionWithTitle:@"确定" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action){
+        [self dismissViewControllerAnimated:YES completion:nil];
+        
+    }];
+    [alertController addAction:okAction];
+    [self presentViewController:alertController animated:YES completion:nil];
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
